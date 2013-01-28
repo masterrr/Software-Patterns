39cf58c25ef31cc06347b8efa4cda1adee6b71a6
Dmitry Kurilo <me@masterrr.name>
2013-01-28 23:51:16 +0300

    Factory Simple Pattern init, a lot of TODO

---
+++ Factory Simple Pattern/src/CarStore.java
@@ -0,0 +1,13 @@
+
+public class CarStore {
+	CarSimpleFactory factory;
+	public CarStore(CarSimpleFactory fac) {
+		this.factory = fac;
+	}
+	
+	public Car buyCar(String model) {
+		Car car;
+		car.testdrive;
+		car.sell;
+	}
+}
